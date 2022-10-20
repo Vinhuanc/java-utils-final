b7b2e059ef86072692cda698383efea634b7c66f
Vinhuanc <huann.chenn@gmail.com>
2022-10-20 10:34:58 -0500

    changed camelProducer and camelConsumer for more reusable. Passing own camelContext and producerTemplate

--- src/main/java/kafka/producer/CamelProducer.java
+++ src/main/java/kafka/producer/CamelProducer.java
@@ -10,31 +10,28 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 public  class CamelProducer {
     public static final Logger LOG = LoggerFactory.getLogger(CamelProducer.class);
-    public CamelProducer() throws IOException {  }
-    public static void main(String[] args) throws Exception {
+    public CamelProducer() throws IOException {}
 
-            CamelContext camelContext = new DefaultCamelContext();
-            LOG.info("starting route:");
-            camelContext.getPropertiesComponent().setLocation("classpath:application.properties");
-            camelContext.addRoutes((new RouteBuilder() {
+    public static void setContext(CamelContext camelContext, String location) throws Exception {
+         LOG.info("starting route:");
+         camelContext.getPropertiesComponent().setLocation(location);
+         camelContext.addRoutes((new RouteBuilder() {
                 @Override
                 public void configure() throws Exception {
                     from("direct:kafkaStart")
-                            .process(new MapToGenericRecord())
-                            .to("kafka:{{producer.topic}}?brokers={{kafka.brokers}}&clientId={{producer.clientId}}&valueSerializer={{value.serializer}}&schemaRegistryURL={{schema.registry.url}}&specificAvroReader=true");
+                            .to("kafka:{{producer.topic}}?brokers={{kafka.brokers}}&clientId={{producer.clientId}}&valueSerializer={{key-serializer-class}}&schemaRegistryURL={{schema.registry.url}}&specificAvroReader=true");
                 }
             }));
             camelContext.start();
+    }
 
-            Students testStudent = new Students();
-            testStudent.setMajor("biology");
-            testStudent.setName("Groot568378dghmyfmdtm");
-            ProducerTemplate producerTemplate = camelContext.createProducerTemplate();
-            Endpoint ep = camelContext.getEndpoint("kafka:{{producer.topic}}?brokers={{kafka.brokers}}");
-            producerTemplate.setDefaultEndpoint(ep);
-            producerTemplate.sendBody(testStudent);
-            Thread.sleep(10L * 60 * 1000);
-        }
+    public static void setTemplate(ProducerTemplate producerTemplate, CamelContext camelContext, Object object) throws InterruptedException {
+        producerTemplate = camelContext.createProducerTemplate();
+        Endpoint ep = camelContext.getEndpoint("kafka:{{producer.topic}}?brokers={{kafka.brokers}}");
+        producerTemplate.setDefaultEndpoint(ep);
+        producerTemplate.sendBody(object);
+        Thread.sleep(10L * 60 * 1000);
+    }
 
 
 }
--- src/test/java/kafka/CamelTest.java
+++ src/test/java/kafka/CamelTest.java
@@ -1,4 +1,5 @@
 package kafka;
+
 import beans.Students;
 import kafka.consumer.CamelConsumer;
 import kafka.producer.CamelProducer;
@@ -14,9 +15,23 @@ public class CamelTest {
     public static final Logger LOG = LoggerFactory.getLogger(CamelTest.class);
 
     @Test
-    public void testCamelConsumer() throws InterruptedException, IllegalAccessException {
-        CamelConsumer.camelConsume();
-    }
+    public void testCamelProduce() throws Exception {
+        CamelContext camelContext = new DefaultCamelContext();
+        CamelProducer.setContext(camelContext,"classpath:application.properties");
+
+        ProducerTemplate producerTemplate = camelContext.createProducerTemplate();
+        Students testStudent = new Students();
+        testStudent.setMajor("biology");
+        testStudent.setName("10/20 10:33am");
+        CamelProducer.setTemplate(producerTemplate, camelContext, testStudent);
 
     }
+    @Test
+    public void testCamelConsumer() throws InterruptedException, IllegalAccessException {
+        CamelContext camelContext = new DefaultCamelContext();
+        CamelConsumer.camelConsume(camelContext,"classpath:application.properties");
+    }
+
+
+}
 
